63db9eae7748ed5f88ea272c7f6cac28c4ef633c
Graßl, Timon <Timon.Grassl@interhyp.de>
2023-01-03 15:44:09 +0100

    feat: add search bar

--- src/pages/history/routes/View/View.tsx
+++ src/pages/history/routes/View/View.tsx
@@ -2,18 +2,26 @@ import { Timeline } from '@pages/history/components/Timeline/Timeline';
 import { useHistory } from '@pages/history/providers/HistoryProvider';
 import { getFaviconUrl } from '@src/utils/history';
 import clsx from 'clsx';
-import * as lz from 'lz-string';
 import { createMemo } from 'solid-js';
 import styles from './View.module.scss';
+import { SearchBar } from '@pages/history/components/SearchBar/SearchBar';
+import { useNavigate } from '@solidjs/router';
 
 export const View = () => {
   const { history, activeItem } = useHistory();
+  const navigate = useNavigate();
+
+  const handleSearch = (query: string) => {
+    if (query === '' || query === ' ') return;
+
+    navigate(`/search/${query}`);
+  };
 
   const previewSrc = createMemo(() => {
     if (!activeItem()) return null;
 
     if (activeItem()?.preview) {
-      return lz.decompressFromUTF16(activeItem().preview);
+      return activeItem().preview;
     }
 
     return getFaviconUrl(activeItem()?.url, 250);
@@ -21,6 +29,7 @@ export const View = () => {
 
   return (
     <>
+      <SearchBar class={styles.search} onAction={handleSearch} />
       {!history.loading && (
         <div class={clsx(styles.preview, !activeItem()?.preview && styles.icon)} style={{ 'border-color': activeItem()?.colors?.hex }}>
           <img src={previewSrc()} />
